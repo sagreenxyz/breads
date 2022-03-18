b0bd70d050beb321836eb2f84d2d3e4726ff9e4b
Scott Green <sagreenxyz@gmail.com>
2022-03-18 12:07:38 -0500

    1.12 configure controllers in application and direct breads routes to the breads controller

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!');
 });
 
+// CONTROLLERS
+const breadsController = require('./controllers/breads_controller.js');
+app.use('/breads', breadsController);
+
 // LISTEN
 app.listen(PORT, () => {
     console.log(`Application running on port ${PORT}:  http://localhost:${PORT}`);
