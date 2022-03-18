1df2b83346515b105601e77faa869883000cb8f5
Scott Green <sagreenxyz@gmail.com>
2022-03-18 13:23:45 -0500

    3.10 configuring middleware to utilize public folder as having static content

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
 
 // ROUTES
 app.get('/', (req, res) => {
