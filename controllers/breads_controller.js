ad5538c6e6dc95bb86a12409dd042f3fc2ca5878
Scott Green <sagreenxyz@gmail.com>
2022-03-18 16:04:05 -0500

    4.2 adding a breads controller handler for creating (posting) new data to the bread model

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -21,4 +21,15 @@ breads.get('/:arrayIndex', (req, res) => {
     };
 });
 
+// CREATE
+breads.post('/', (req, res) => {
+    if(req.body.hasGluten === 'on') {
+        req.body.hasGluten = 'true';
+    } else {
+        req.body.hasGluten = 'false';
+    };
+    Bread.push(req.body);
+    res.send(Bread);
+});
+
 module.exports = breads;
