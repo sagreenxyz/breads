61e880d522c4a3aa4a783fffa44754abc2d661cc
Scott Green <sagreenxyz@gmail.com>
2022-03-18 16:18:19 -0500

    4.12 submit a default image in bread post if one not provided

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -28,7 +28,10 @@ breads.get('/:arrayIndex', (req, res) => {
 
 // CREATE
 breads.post('/', (req, res) => {
-    if(req.body.hasGluten === 'on') {
+    if (!req.body.image) {
+        req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    };
+    if (req.body.hasGluten === 'on') {
         req.body.hasGluten = 'true';
     } else {
         req.body.hasGluten = 'false';
