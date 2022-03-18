41712256caf2318773f2fc12ad352c1fc5ee181c
Scott Green <sagreenxyz@gmail.com>
2022-03-18 13:01:23 -0500

    3.3 passing bread parameter into show view and console logging to establish it will work (does not work yet - need to update controller route handler)

--- views/show.jsx
+++ views/show.jsx
@@ -1,8 +1,12 @@
 const React = require('react');
+const Default = require('./layouts/Default');
 
-function Show() {
+function Show({bread}) {
+    console.log(bread.name);
     return (
-
+        <Default>
+            <h2>Show Page</h2>
+        </Default>
     );
 };
 
