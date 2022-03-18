779d98d242227166795c41eb1373d6c176c50b42
Scott Green <sagreenxyz@gmail.com>
2022-03-18 13:15:19 -0500

    3.6 ternary operator in show view to conditionally render text

--- views/show.jsx
+++ views/show.jsx
@@ -6,6 +6,15 @@ function Show({bread}) {
         <Default>
             <h2>Show Page</h2>
             <h3>{bread.name}</h3>
+            <p>
+                and it
+                {
+                    bread.hasGluten
+                    ? <span> does </span>
+                    : <span> does NOT </span>
+                }
+                have gluten.
+            </p>
             <img src={bread.image} alt={bread.name} />
         </Default>
     );
