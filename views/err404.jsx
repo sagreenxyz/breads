3895788276b9270563d0ff6ebf8635deec3fc091
Scott Green <sagreenxyz@gmail.com>
2022-03-18 15:50:06 -0500

    3.18 adding content to the err404 view

--- views/err404.jsx
+++ views/err404.jsx
@@ -4,7 +4,7 @@ const Default = require('./layouts/Default');
 function Err404() {
     return (
         <Default>
-            
+            <h2>404 Error - Page Not Found</h2>
         </Default>
     );
 };
