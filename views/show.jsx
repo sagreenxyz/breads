c6f6fd7dcc099bd506b44c549268f5e4df7993f7
Scott Green <sagreenxyz@gmail.com>
2022-03-18 13:12:19 -0500

    3.5 rendering data in the show view

--- views/show.jsx
+++ views/show.jsx
@@ -2,10 +2,11 @@ const React = require('react');
 const Default = require('./layouts/Default');
 
 function Show({bread}) {
-    console.log(bread.name);
     return (
         <Default>
             <h2>Show Page</h2>
+            <h3>{bread.name}</h3>
+            <img src={bread.image} alt={bread.name} />
         </Default>
     );
 };
