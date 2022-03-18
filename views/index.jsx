8adc20dfdab8401c177eaebd9765d622ba098a2e
Scott Green <sagreenxyz@gmail.com>
2022-03-18 12:41:49 -0500

    2.17 using .map() on breads array to present items as a list

--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,17 @@ function Index({breads}) {
     return (
         <Default>
             <h2>Index Page</h2>
-            <p>I have {breads[0].name} bread!</p>
+            <ul>
+                {
+                    breads.map((bread, index) => {
+                        return (
+                            <li key={index}>
+                                {bread.name}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </Default>
     );
 };
