d6f289abe4f8b343bd91e1fa23e70ee2a999029b
Scott Green <sagreenxyz@gmail.com>
2022-03-18 11:57:15 -0500

    1.09 configure express object for the app, set route for homepage, and set listening to configured PORT

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ const express = require('express');
 
 // CONFIGURATION
 const PORT = process.env.PORT;
-console.log(`Application port is ${PORT}`);
+const app = express();
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!');
+});
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Application running on port ${PORT}:  http://localhost:${PORT}`);
+});
