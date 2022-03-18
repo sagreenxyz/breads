8196e835b2a2fead8f81209286086785bba052e7
Scott Green <sagreenxyz@gmail.com>
2022-03-18 12:15:17 -0500

    1.17 configure breads show route with array index from path parameter supplied by user

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,4 +7,9 @@ breads.get('/', (req, res) => {
     res.send(Bread);
 });
 
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex]);
+});
+
 module.exports = breads;
